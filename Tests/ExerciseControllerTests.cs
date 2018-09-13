4f2f6fdcadcaef010b0f1e50ef856f1d9e919938
rutiini <rutiluz88@gmail.com>
2018-09-14 01:15:24 +0300

    fix for missing ;

--- Tests/ExerciseControllerTests.cs
+++ Tests/ExerciseControllerTests.cs
@@ -149,7 +149,7 @@ namespace App.Tests
       var payload = result.Value as Treenirepository.Models.Exercise;
 
       // check the returned objects references and integrity
-      Assert.True(payload.Name == exerciseName, "Exercise name was not what expected")
+      Assert.True(payload.Name == exerciseName, "Exercise name was not what expected");
       Assert.Equal(payload.Sections.Count,2);
       Assert.True(payload.Sections.First().ExerciseId == payload.Id, "Section link is mapped wrong or missing");
     }
